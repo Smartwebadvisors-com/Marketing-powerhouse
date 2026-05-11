4354d383fe87c12b2c8f635dfa2086b6b88d59d3
root <root@srv1658836.hstgr.cloud>
2026-05-11 09:24:11 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const D = {
+  bg:"#080E1A",bgCard:"#0D1525",bgHover:"#111D30",bgActive:"#142038",
+  border:"#1A2D4A",borderHi:"#234068",teal:"#00D4B0",tealDim:"#00A88C",
+  tealGlow:"rgba(0,212,176,0.12)",white:"#EEF4FF",whiteMid:"#8BA3C4",
+  whiteDim:"#4A6080",red:"#FF5757",amber:"#F5A623",green:"#22C55E",
+  purple:"#9D7AFA",blue:"#3B9EFF",
+};
